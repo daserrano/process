6a2dac6a23266f48ccdd67bd70ab2084fa0fe1d2
dam <pc07@pc07-VirtualBox.(none)>
2014-10-03 10:51:23 +0200

    not yet

--- 06_exec/exec.cpp
+++ 06_exec/exec.cpp
@@ -2,51 +2,54 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#define N 10
 
 
 int spawn(const char* program, char** arg_list)
 {
-    pid_t child_pid=0;
+	pid_t child_pid=0;
+	child_pid = fork();
 
-    child_pid = fork();
-    if(child_pid == 0){
-	execvp(program, arg_list);
-	fprintf(stderr, "Error with execvp\n");
-	abort();
-    }
-    else
-	return child_pid;
+	if(child_pid == 0){
+		execvp(program, arg_list);
+		fprintf(stderr, "Error with execvp\n");
+		abort();
+	}
+	else
+		return child_pid;
 }
 
 int main(int argc, char *argv[])
 {
-    int opcion;
-    char *arg_list[] = {
-	"www.google.es",
-	NULL
-    };
+	int opcion;
 
-    printf("Bienvenido Compañero.\n\n");
-    while(opcion!=3)
-    {
-	printf("MENU\n");
-	printf("1. Ls\n");
-	printf("2. Google\n");
-	printf("3. Salir\n");
-	scanf("%i", &opcion);
-
-	switch(opcion)
+	while(opcion!=3)
 	{
-	    case 1:
-		printf("ls");
-		break;
-	    case 2:
-		spawn("firefox", arg_list);
-		break;
-	    case 3:
-		return EXIT_SUCCESS;
-		break;
+		system("clear");
+		printf("MENU\n");
+		printf("1. Ls\n");
+		printf("2. Google\n");
+		printf("3. Salir\n");
+		scanf("%i", &opcion);
+
+		switch(opcion)
+		{
+			case 1:
+				spawn("ls", NULL);
+				break;
+			case 2:
+				char *arg_list[] = {
+					"firefox",
+					"www.google.es",
+					"\",
+					NULL
+				};
+				spawn("firefox", arg_list);
+				break;
+			case 3:
+				return EXIT_SUCCESS;
+				break;
+		}
 	}
-    }
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
