1c5178ede35b87188346d040a929df0336f76e83
David <david@david-VirtualBox.(none)>
2014-10-05 16:59:17 +0200

    ok, but not yet

--- 06_exec/exec.cpp
+++ 06_exec/exec.cpp
@@ -7,49 +7,58 @@
 
 int spawn(const char* program, char** arg_list)
 {
-	pid_t child_pid=0;
-	child_pid = fork();
+    pid_t child_pid=0;
+    child_pid = fork();
 
-	if(child_pid == 0){
-		execvp(program, arg_list);
-		fprintf(stderr, "Error with execvp\n");
-		abort();
-	}
-	else
-		return child_pid;
+    if(child_pid == 0){
+	execvp(program, arg_list);
+	fprintf(stderr, "Error with execvp\n");
+	abort();
+    }
+    else
+	return child_pid;
 }
 
 int main(int argc, char *argv[])
 {
-	int opcion;
+    int opcion;
+    char *arguments;
 
-	while(opcion!=3)
+    while(opcion!=3)
+    {
+	system("clear");
+	printf("MENU\n");
+	printf("1. Ls\n");
+	printf("2. Google\n");
+	printf("3. Salir\n");
+	scanf("%i", &opcion);
+
+	switch(opcion)
 	{
-		system("clear");
-		printf("MENU\n");
-		printf("1. Ls\n");
-		printf("2. Google\n");
-		printf("3. Salir\n");
-		scanf("%i", &opcion);
-
-		switch(opcion)
+	    case 1:
 		{
-			case 1:
-				spawn("ls", NULL);
-				break;
-			case 2:
-				char *arg_list[] = {
-					"firefox",
-					"www.google.es",
-					"\",
-					NULL
-				};
-				spawn("firefox", arg_list);
-				break;
-			case 3:
-				return EXIT_SUCCESS;
-				break;
+		    char *arg_list[] = {
+			"ls",
+			"-l",
+			"../",
+			NULL
+		    };
+		    spawn("ls", arg_list);
 		}
+		break;
+	    case 2:
+		{
+		    char *arg_list[] = {
+			"firefox",
+			"www.google.es",
+			NULL
+		    };
+		    spawn("firefox", arg_list);
+		}
+		break;
+	    case 3:
+		return EXIT_SUCCESS;
+		break;
 	}
-	return EXIT_SUCCESS;
+    }
 }
