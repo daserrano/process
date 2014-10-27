07a82eb92369f69d30d15402088853d8d8629fc3
David <david@david-VirtualBox.(none)>
2014-10-27 21:12:36 +0100

    do 10000

--- 07_threads/hilos2.cpp
+++ 07_threads/hilos2.cpp
@@ -1,28 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
 
 struct Tnumero {
 
     int numero;
+    int aleatorio;
+    int id;
 };
 
 void *operar(void *arg){
 
-    struct Tnumero *n;
-    n = (struct Tnumero *)arg;
+    struct Tnumero *n = (struct Tnumero *)arg;
 
+    while(n -> numero < 10000){
+	if(n->id = 1){
+	    n -> aleatorio = rand() % 4;
+	    n -> numero += n -> aleatorio;
+	    printf("%i ", n -> numero);
+	}
+
+	if(n->id = 2){
+	    n -> aleatorio = rand() % 3; 
+	    n -> numero += n -> aleatorio;
+	    printf("%i ", n -> numero);
+	}
+    }
 }
 
 int main(int argc, char *argv[]){
 
     pthread_t thread1, thread2;
     struct Tnumero num;
+    num.numero    = 1;
+    num.aleatorio = 0;
+
+    srand(time(NULL));
 
     if(thread1)
-	num.numero = rand() % 4;
+	num.id = 1;
     else
-	num.numero = rand() % 3;
+	num.id = 2;
 
     pthread_create(&thread1, NULL, operar,(void *)&num); 
     pthread_create(&thread2, NULL, operar,(void *)&num); 
@@ -30,5 +49,6 @@ int main(int argc, char *argv[]){
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
 
-    return EXIT_SUCCESS;
+    printf("\n");
+
 }
